e08607660328e1b06955596747fa0e47cee38d55
unknown <necha082@gmail.com>
2022-11-01 03:56:13 +0700

    navigation

---
+++ navigation_route/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'secondpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/secondpage',
+      routes: {
+        '*/secondpage': (context) => SecondPage(),
+      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Navigation Route"),
+        ),
+        body: Builder(
+            builder: (context) => Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      MaterialButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/secondpage');
+                          },
+                          color: Colors.blue,
+                          child: Text("Pergi ke Halaman 2",
+                              style: TextStyle(color: Colors.white))),
+                    ], // <widget>[]
+                  ), // Column
+                )), // Center // Builder
+      ), // Scaffold
+    );
+  }
+} // MaterialApp
---
+++ navigation_route/lib/secondpage.dart
@@ -0,0 +1,27 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  // const SecondPage({ Key? key }): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Second Page")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            MaterialButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.blue,
+              child: Text("Kembali", style: TextStyle(color: Colors.white)),
+            )
+          ], // <widget>[]
+        ),
+      ),
+    ); // Center
+  }
+}
